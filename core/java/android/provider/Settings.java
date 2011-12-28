532b2954ee3e8f0c2b09001a8027c96f6797dec6
nebkat <nebkat@teamhacksung.org>
2011-12-28 11:43:04 +0000

    Hide Adb Notification (framework)
    Change-Id: Ie5325b3c6cde64be8d36f6afc4e747addb5ce8df

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2681,6 +2681,12 @@ public final class Settings {
         public static final String ADB_PORT = "adb_port";
 
         /**
+         * Whether to display the ADB notification.
+         */
+        public static final String ADB_NOTIFY = "adb_notify";
+
+
+        /**
          * Setting to allow mock locations and location provider status to be injected into the
          * LocationManager service for testing purposes during application development.  These
          * locations and status values  override actual location and status information generated
