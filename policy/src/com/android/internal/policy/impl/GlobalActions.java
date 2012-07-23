ff76ef5ab397295a93e70dc0d18b4c8fe0391cd1
Chirayu Desai <chirayudesai1@gmail.com>
2012-07-23 17:41:34 +0530

    GlobalActions: make silent mode the last toggle
    if multi user support is enabled, silent mode toggle is above
    the user list, but it should be the last.
    
    Change-Id: I7d6146c740827c856e3c8d39d349534dfbfee12d

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -319,11 +319,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         // next: airplane mode
         mItems.add(mAirplaneModeOn);
 
-        // last: silent mode
-        if (SHOW_SILENT_TOGGLE) {
-            mItems.add(mSilentModeAction);
-        }
-
+        // next: users
         List<UserInfo> users = mContext.getPackageManager().getUsers();
         if (users.size() > 1) {
             UserInfo currentUser;
@@ -358,6 +354,11 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 };
                 mItems.add(switchToUser);
             }
+        }
+
+        // last: silent mode
+        if (SHOW_SILENT_TOGGLE) {
+            mItems.add(mSilentModeAction);
         }
 
         mAdapter = new MyAdapter();
