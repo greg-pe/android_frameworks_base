5c32fb730e2d9ef82676dd2b427027dc050a92d8
Roman Birg <romanbirg@gmail.com>
2012-12-13 23:09:15 -0600

    SystemUI: refactor fast charge toggle
    Device trees need to set a path to enable the toggle.
    It will not show otherwise.
    
    Moved all file operations off the main thread and use a
    preference to save the last state so it persists.
    
    	<!-- empty string to disable, or like: /sys/kernel/fast_charge/force_fast_charge
             it is disabled by default so devices need to add an overlay to enable-->
        <string name="config_fastChargePath" translatable="false"></string>
    
    PS8: Lower delay to 250
    
    Change-Id: Ib1465a98893ec8e7854c8b80f8352016ff6248ad
    Signed-off-by: Roman Birg <romanbirg@gmail.com>
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Prefs.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Prefs.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Prefs.java
@@ -29,11 +29,29 @@ public class Prefs {
     public static final String SHOWN_COMPAT_MODE_HELP = "shown_compat_mode_help";
     public static final String SHOWN_QUICK_SETTINGS_HELP = "shown_quick_settings_help";
 
+    public static final String LAST_FCHARGE_STATE = "last_fcharge_state";
+
     public static SharedPreferences read(Context context) {
         return context.getSharedPreferences(Prefs.SHARED_PREFS_NAME, Context.MODE_PRIVATE);
     }
 
     public static SharedPreferences.Editor edit(Context context) {
         return context.getSharedPreferences(Prefs.SHARED_PREFS_NAME, Context.MODE_PRIVATE).edit();
+    }
+
+    public static void setLastBatteryLevel(Context context, int level) {
+        edit(context).putInt(LAST_BATTERY_LEVEL, level).commit();
+    }
+
+    public static int getLastBatteryLevel(Context context) {
+        return read(context).getInt(LAST_BATTERY_LEVEL, 50);
+    }
+
+    public static void setLastFastChargeState(Context context, boolean enabled) {
+        edit(context).putBoolean(LAST_FCHARGE_STATE, enabled).commit();
+    }
+
+    public static boolean getLastFastChargeState(Context context) {
+        return read(context).getBoolean(LAST_FCHARGE_STATE, false);
     }
 }
