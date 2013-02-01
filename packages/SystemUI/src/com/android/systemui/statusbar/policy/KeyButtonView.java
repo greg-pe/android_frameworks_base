62deb778f233d9782543cf8a6d45bd1a9b108cf2
Danesh Mondegarian <daneshm90@gmail.com>
2013-02-01 17:00:22 -0500

    Navbar : Fix left over glow
    Invalidate all buttons, to avoid nearby glow interference
    
    Change-Id: I83ab05b91bd5db70390a5c9480513768864a2be7

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -229,7 +229,7 @@ public class KeyButtonView extends ImageView {
 
             // also invalidate our immediate parent to help avoid situations where nearby glows
             // interfere
-            ((View)getParent()).invalidate();
+            ((View)getParent().getParent()).invalidate();
         }
     }
 
