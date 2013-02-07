b803ced128c5eecac44e8ee3d70496fd829ed29b
buckmarble <forrestmarble@gmail.com>
2013-02-07 12:28:40 -0500

    FIX: status bar center/right/none options were broken and pull down had duplicate clock at top after pie commit this is a fix for both

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -103,6 +103,10 @@ public class Clock extends TextView {
 
     public Clock(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mHandler = new Handler();
+        SettingsObserver settingsObserver = new SettingsObserver(mHandler);
+        settingsObserver.observe();
+        updateSettings();
     }
 
     @Override
